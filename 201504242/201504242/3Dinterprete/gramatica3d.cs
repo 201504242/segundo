b2345ad0163f1102ffdfd1b1cbd753a3a2eb1829
jorged104 <nowelljorge@gmail.com>
2019-07-10 18:31:34 -0600

    primeros cambios

---
+++ 201504242/201504242/3Dinterprete/gramatica3d.cs
@@ -0,0 +1,70 @@
+﻿using Irony.Parsing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _201504242._3Dinterprete
+{
+    class gramatica3d : Grammar
+    {
+        public gramatica3d()
+            : base(false)
+        {
+
+            #region Aritmaticas
+            Terminal pa_A = ToTerm("("),
+                    pa_C = ToTerm(")"),
+                    masmas = ToTerm("++"),
+                    menosmenos = ToTerm("--"),
+                    modulo = ToTerm("%"),
+                    mas = ToTerm("+"),
+                    menos = ToTerm("-"),
+                    por = ToTerm("*"),
+                    div = ToTerm("/"),
+                    potencia = ToTerm("^")
+                    ;
+            #endregion
+            #region   Relacionales
+            Terminal identico = ToTerm("=="),
+                     diferente = ToTerm("!="),
+                     menorQue = ToTerm("<"),
+                     mayorQue = ToTerm(">"),
+                     menorIgual = ToTerm("<="),
+                     mayorIgual = ToTerm(">=");
+            #endregion
+
+            #region Logicas
+            Terminal and = ToTerm("&&"),
+                     or = ToTerm("||"),
+                     not = ToTerm("!");
+            #endregion
+
+            #region Reservadas
+            Terminal crear = ToTerm("crear"),
+                tabla = ToTerm("tabla");
+            #endregion
+
+            #region  Tipos de datos
+
+            IdentifierTerminal id = new IdentifierTerminal("IDENTIFER");
+            id.Name = "identificador";
+            NumberLiteral numero = new NumberLiteral("numero");
+            numero.Name = "numero";
+            StringLiteral cadena = new StringLiteral("cadena", "\"", StringOptions.AllowsAllEscapes);
+            cadena.Name = "cadena";
+
+            RegexBasedTerminal bolean = new RegexBasedTerminal("(verdadero|falso)");
+            bolean.Name = "bool";
+            #endregion
+
+            #region NonTerminals
+            //NonTerminal S = new NonTerminal("S", typeof(AstNode));
+            #endregion
+
+           
+
+        }
+    }
+}
